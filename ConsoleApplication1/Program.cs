bd03d07ae23b1b5c6fd84b0e12dee57c3c8ff941
yurij-kupreev <yurij.kupreev@gmail.com>
2015-03-22 15:18:23 +0300

    Day 7 Tasks

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task3_BookService;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book a = new Book("Tolstoy", "War and Peace", 1865, "St.Petersburg");
+            Book b = new Book("Lermontov", "Hero of Our Time", 1840, "St.Petersburg");
+            Book c = new Book("Lermontov", "Daemon", 1842, "Moscow");
+
+            List<Book> bookList = new List<Book>();
+            bookList.Add(a);
+            bookList.Add(b);
+            bookList.Add(c);
+            BookListService.SortBooks(bookList, new AuthorsDecrease());
+            foreach (Book element in bookList)
+            {
+                Console.WriteLine(element.ToString());
+            }
+            Console.WriteLine();
+            List<Book> bookList3 = BookListService.GiveBooksToParameter(bookList, publishedBy:"St.Petersburg");
+            foreach (Book element in bookList3)
+            {
+                Console.WriteLine(element.ToString());
+            }
+
+            //BinaryFileRepository bfr = new BinaryFileRepository("fileBooks");
+            //bfr.SaveBooks(bookList);
+
+            //List<Book> bookList2 = (List<Book>)bfr.LoadBooks();
+            //foreach (Book element in bookList2)
+            //{
+            //    Console.WriteLine(element.ToString());
+            //}
+            //bfr.SaveBooks(bookList);
+            //bookList2 = (List<Book>)bfr.LoadBooks();
+            //foreach (Book element in bookList2)
+            //{
+            //    Console.WriteLine(element.ToString());
+            //}
+        }
+    }
+}
--- Task2/BubbleSort.cs
+++ Task2/BubbleSort.cs
@@ -10,13 +10,13 @@ namespace Task2
     {
         public static void Sort(int[][] matrix, IArrayComparer sortmodes)
         {
-            int[] matrixBuffer;
             for (int i = 0; i < matrix.Length; ++i)
             {
                 for (int j = i + 1; j < matrix.Length; ++j)
                 {
                     if (sortmodes.Compare(matrix[i], matrix[j]) > 0)
                     {
+                        int[] matrixBuffer;
                         matrixBuffer = matrix[i];
                         matrix[i] = matrix[j];
                         matrix[j] = matrixBuffer;
@@ -27,13 +27,13 @@ namespace Task2
 
         public static void Sort(int[][] matrix, SortMatrixWithModes.ArrayCompareDelegate compareMethod)
         {
-            int[] matrixBuffer;
             for (int i = 0; i < matrix.Length; ++i)
             {
                 for (int j = i + 1; j < matrix.Length; ++j)
                 {
                     if (compareMethod(matrix[i], matrix[j]) > 0)
                     {
+                        int[] matrixBuffer;
                         matrixBuffer = matrix[i];
                         matrix[i] = matrix[j];
                         matrix[j] = matrixBuffer;
